197fa002714e5eb46b5bc07c8327c629d471f48d
Greta Moseson <gmoseson@gmail.com>
2015-04-06 00:12:50 -0400

    Add simple form gem and views.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,30 @@
 class StoriesController < ApplicationController
-  def index
-  end
+	before_action :authenticate_user!
 
-  def new
-  end
+	def index
+		# @prompt = Prompt.all.order('RAND()').first
+		@prompts = Prompt.all
+		@stories = Story.all
+	end
+
+	def new
+		@story = Story.new
+		@prompt = Prompt.find(params[:prompt_id])
+	end
+
+	def create
+		@story = Story.new(story_params)
+		@story.user = current_user
+
+		if @story.save
+			redirect_to stories_path
+		else
+			render :new
+		end
+	end
+
+	def story_params
+		params.require(:story).permit(:body)
+	end
+
 end
